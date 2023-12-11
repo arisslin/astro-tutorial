a8803e77ebc8f391c5346cea9be785278f6d8901
Andreas Rissling <a.rissling@steinberg.de>
2023-12-11 17:34:45 +0100

    Config prettier for astro

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
